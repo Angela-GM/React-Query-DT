bae07dab7821dfbd465419a8da6b1312e80e2531
Angela-GM <angelagm0112@gmail.com>
2023-11-09 15:31:56 +0100

    agregar y filtrar por labels

---
+++ 02-react-query-issues-main/src/helpers/index.ts
@@ -0,0 +1,5 @@
+export { sleep } from "./sleep";
+export { timeSince } from "./time-since";
+
+
+
--- 02-react-query-issues-main/src/issues/hooks/useIssues.tsx
+++ 02-react-query-issues-main/src/issues/hooks/useIssues.tsx
@@ -1,16 +1,40 @@
 import { useQuery } from "react-query";
 import { githubApi } from "../../api/githubApi";
-import { Issue } from "../interfaces";
+import { Issue, State } from "../interfaces";
 import { sleep } from "../../helpers/sleep";
+import { FC } from "react";
 
-const getIssues = async (): Promise<Issue[]> => {
+interface Props {
+  state?: State;
+  labels: string[];
+}
+
+const getIssues = async ( labels:string[] = [], state?: State ): Promise<Issue[]> => {
   await sleep(2)
-  const { data } = await githubApi.get<Issue[]>("/issues");
+
+  const params = new URLSearchParams(); //extraer la url
+
+  if (state) params.append('state', state)
+
+  if ( labels.length > 0) {
+    const labelString =  labels.join(',');
+    params.append('labels', labelString);
+  }
+
+  params.append('page', '1');
+  params.append('per_page', '5');
+
+
+
+  const { data } = await githubApi.get<Issue[]>("/issues", { params });
   return data;
 };
 
-export const useIssues = () => {
-  const issuesQuery = useQuery(["issues"], getIssues);
+export const useIssues = ({ state, labels }: Props) => {
+  const issuesQuery = useQuery(
+    ["issues", { state, labels } ],
+    () => getIssues(labels, state)
+    );
 
   return {
     issuesQuery,
