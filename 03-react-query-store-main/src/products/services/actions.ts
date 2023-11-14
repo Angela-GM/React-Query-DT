c0f7feaa421d37181a26aae07b47f195fb15a47e
Angela-GM <angelagm0112@gmail.com>
2023-11-14 19:09:37 +0100

    mostrar productos en el listado, y añadir filtros de mujer y hombre, todo con react query

---
+++ 03-react-query-store-main/src/products/services/actions.ts
@@ -0,0 +1,27 @@
+import { type Product, productsApi } from "..";
+
+interface GetProductsOptions {
+  filterKey?: string;
+}
+
+const sleep = (seconds: number):Promise<boolean> => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(true);
+      
+    }, seconds * 1000);
+  })
+
+}
+
+export const getProducts = async ({ filterKey }: GetProductsOptions) => {
+
+  await sleep(2)
+
+  const filterUrl = (filterKey) ? `category=${filterKey}`: '';
+
+    const { data } = await productsApi.get<Product[]>(`/products?${filterUrl}`);
+    return data;
+}
+
+    
