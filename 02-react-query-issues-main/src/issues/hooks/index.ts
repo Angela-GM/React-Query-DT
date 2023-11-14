7aef28b30215bb7c015984d4211c305e44545982
Angela-GM <angelagm0112@gmail.com>
2023-11-14 12:49:13 +0100

    carga infinita

--- 02-react-query-issues-main/src/issues/hooks/index.ts
+++ 02-react-query-issues-main/src/issues/hooks/index.ts
@@ -1,6 +1,8 @@
+
 export { useIssue } from "./useIssue";
 export { useIssues } from "./useIssues";
 export { useLabels } from "./useLabels";
+export { useIssuesInfinite } from "./useIssuesInfinite";
 
 
 
