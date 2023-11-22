b950eca64cd0856c948952eceaa8120dd382aac1
Angela-GM <angelagm0112@gmail.com>
2023-11-22 13:59:35 +0100

    crear validaciones de formulario con React Hook Form

--- 03-react-query-store-main/src/products/index.ts
+++ 03-react-query-store-main/src/products/index.ts
@@ -3,6 +3,8 @@ export { productsApi } from './api/productsApi';
 
 export { ProductCard } from './components/ProductCard';
 export { ProductList } from './components/ProductList';
+
+// Hooks
 export { useProducts } from '../hooks/useProducts';
 export { useProduct } from '../hooks/useProduct';
 export { usePrefetchProduct } from '../hooks/usePreFetchProduct';
@@ -11,7 +13,6 @@ export { usePrefetchProduct } from '../hooks/usePreFetchProduct';
 
 
 export type { Product } from './interfaces/product'; //poner la palabra type para forzar y decir que lo que exporto es una interfaz
-
 
 export { StoreLayout } from './layout/StoreLayout';
 
